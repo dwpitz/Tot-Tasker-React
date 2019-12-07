555b2cdbe5931e96815a4815e9f2fa2b37da47eb
David Pitz <dw_pitz@yahoo.com>
2019-12-07 12:43:20 -0600

    added login fields

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -1,5 +1,5 @@
 import React, { Component} from "react";
-import { Form, Header } from "semantic-ui-react";
+import { Header, Form, Label, Divider } from "semantic-ui-react";
 
 class LoginRegisterForm extends Component {
 	constructor() {
@@ -19,6 +19,31 @@ class LoginRegisterForm extends Component {
 				<Header>
 					<h1> Tot Tasker </h1>
 				</Header>
+				<Label>Family Name:</Label>
+				<Form.Input
+              		type="text"
+              		name="familyName"
+              		// value={this.state.full_name}
+              		// onChange={this.handleChange}
+            ></Form.Input>
+
+				<Divider hidden />
+				<Label>Email:</Label>
+				<Form.Input
+              		type="text"
+              		name="email"
+              		// value={this.state.full_name}
+              		// onChange={this.handleChange}
+            ></Form.Input>
+				<Divider hidden />
+				<Label>Password:</Label>
+				<Form.Input
+              		type="text"
+              		name="password"
+              		// value={this.state.full_name}
+              		// onChange={this.handleChange}
+            ></Form.Input>
+				<Divider hidden />	
 			</div>
 		)
 	}
